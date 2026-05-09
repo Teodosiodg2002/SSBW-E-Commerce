a5b99d7294fb1773b918a44e52366ea57b08669b
Teodosio Donaire González <teodonaire@gmail.com>
2026-05-09 12:40:52 +0200

    fix(backend): corregido SyntaxError en ESM usando 'import type' para tipos de Express

--- controllers/api.controller.ts
+++ controllers/api.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import type { Request, Response } from 'express';
 import prisma from '../prisma/prisma.client.ts';
 
 export const getProductos = async (req: Request, res: Response) => {
